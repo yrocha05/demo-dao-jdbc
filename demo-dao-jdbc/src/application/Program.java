81917ebbb948b1b48ae12a6b808944dcf937c415
Yuri Rocha <130370083+yrocha05@users.noreply.github.com>
2026-09-17 07:32:08 -0300

    findAll implementation

--- demo-dao-jdbc/src/application/Program.java
+++ demo-dao-jdbc/src/application/Program.java
@@ -17,10 +17,16 @@ public class Program {
 
       System.out.println(seller);
 
-      System.out.println("\n=== TEST 1: Seller findByDepartment ===");
+      System.out.println("\n=== TEST 2: Seller findByDepartment ===");
       Department department = new Department(2,null);
       List<Seller> list = sellerDao.findByDepartment(department);
 
+      for (Seller obj : list){
+        System.out.println(obj);
+      }
+
+      System.out.println("\n=== TEST 3: Seller findAll ===");
+      list = sellerDao.findAll();
       for (Seller obj : list){
         System.out.println(obj);
       }
