a0e99bd44c59cbd39d79e5f682625b83a56f0c35
Yuri Rocha <130370083+yrocha05@users.noreply.github.com>
2026-09-18 14:56:02 -0300

    Delete Implementation

--- demo-dao-jdbc/src/application/Program.java
+++ demo-dao-jdbc/src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -10,6 +11,7 @@ import model.entities.Seller;
 
 public class Program {
     public static void main(String[] args) throws Exception {
+      Scanner sc = new Scanner(System.in);
 
       SellerDao sellerDao = DaoFactory.createSellerDao();
 
@@ -43,6 +45,15 @@ public class Program {
       seller.setName("Martha Waine");
       sellerDao.update(seller);
       System.out.println("Update Completed!");
+
+      System.out.println("\n=== TEST 6: Seller DELETE ===");
+      
+      System.out.println("Enter id for Delete test: ");
+      int id = sc.nextInt();
+      sellerDao.deleteById(id);
+      System.out.println("Delete Completed");
+
+      sc.close();
 
 
     }    
